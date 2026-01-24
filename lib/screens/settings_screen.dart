ccbcb9e02920db4c79558b3928d2571227839a12
Matheus Wendell Dantas Bezerra <matheuswdb1703@gmail.com>
2026-01-24 17:57:38 -0300

    style: Adicionado SafeArea

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -7,50 +7,52 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Configurações')),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          padding: EdgeInsets.all(18.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            spacing: 8.0,
-            children: [
-              Text(
-                'Segurança dos Dados',
-                style: TextStyle(fontWeight: FontWeight.bold),
-                textAlign: TextAlign.start,
-              ),
-              Text(
-                'O "Onde Parei?" guarda tudo localmente. Use as opções abaixo para não perder seus dados ao trocar ou formatar o dispositivo.',
-                style: TextStyle(),
-                textAlign: TextAlign.start,
-              ),
-              Column(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      spacing: 8.0,
-                      children: [Icon(Icons.download), Text('Baixar Backup')],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            padding: EdgeInsets.all(18.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              spacing: 8.0,
+              children: [
+                Text(
+                  'Segurança dos Dados',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.start,
+                ),
+                Text(
+                  'O "Onde Parei?" guarda tudo localmente. Use as opções abaixo para não perder seus dados ao trocar ou formatar o dispositivo.',
+                  style: TextStyle(),
+                  textAlign: TextAlign.start,
+                ),
+                Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        spacing: 8.0,
+                        children: [Icon(Icons.download), Text('Baixar Backup')],
+                      ),
                     ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      spacing: 8.0,
-                      children: [Icon(Icons.upload), Text('Carregar Dados')],
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        spacing: 8.0,
+                        children: [Icon(Icons.upload), Text('Carregar Dados')],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
